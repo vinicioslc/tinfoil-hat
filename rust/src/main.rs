e898019b88014fd3369c7c5fee51d7da35c2c07b
Vinicios Clarindo <vinicioslc@outlook.com>
2023-08-04 14:55:49 -0300

    wip: add more rust code to serve files

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,11 @@
+use std::net::SocketAddr;
+
 use axum::routing::get;
-use axum::Router;
+use axum::{handler::HandlerWithoutStateExt, http::StatusCode, routing::get, Router};
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::TraceLayer,
+};
 
 mod errors;
 
@@ -15,8 +21,23 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    tokio::join!(serve(using_serve_dir(), 3001));
 }
 
 async fn root() -> String {
     return "Its online!".to_string();
+}
+
+async fn serve(app: Router, port: u16) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app.layer(TraceLayer::new_for_http()))
+        .await
+        .unwrap();
+}
+fn using_serve_dir() -> Router {
+    // serve the file in the "assets" directory under `/assets`
+    Router::new().nest_service("/assets", ServeDir::new("assets"))
 }
--- rust/src/routes/shop.rs
+++ rust/src/routes/shop.rs
@@ -1,30 +1,28 @@
 use glob::glob;
 use serde_json::Value;
-use std::{
-    any, env, fs,
-    path::{Path, PathBuf},
-};
+use std::collections::HashMap;
+use std::{fs, path::PathBuf};
 
 use axum::{routing::get, Router};
 
-use crate::errors::{Error, InternalError};
+use crate::errors::Error;
 
 pub fn create_route() -> Router {
     Router::new().route("/shop.json", get(shop_json))
 }
 
 async fn shop_json() -> Result<String, Error> {
-    let mut template = env::current_dir().unwrap();
-    template.push("shop_template.jsonc");
-    println!("opened: {}", template.to_str().unwrap());
+    let mut template_json_path = std::env::current_dir().unwrap();
+    template_json_path.push("shop_template.jsonc");
+    println!("opened: {}", template_json_path.to_str().unwrap());
 
-    match fs::read_to_string(template) {
+    match fs::read_to_string(template_json_path) {
         Ok(current_content) => {
             println!("total characters: {}", current_content.len());
-            let stringuee = current_content.as_str();
-            match json5::from_str::<serde_json::Value>(stringuee) {
-                Ok(parsed_template) => {
-                    return Ok(build_json(parsed_template).await);
+            let template_string = current_content.as_str();
+            match json5::from_str::<serde_json::Value>(template_string) {
+                Ok(template_parsed) => {
+                    return Ok(build_json(template_parsed).await);
                 }
                 Err(e) => {
                     println!("Erro ao ler o arquivo: {}", e);
@@ -49,20 +47,34 @@ pub struct TinfoilResponse {
 async fn list_roms_files() -> Result<TinfoilResponse, Error> {
     let mut dirs: Vec<PathBuf> = Vec::new();
     let mut files: Vec<PathBuf> = Vec::new();
-    let path_to_search = env::current_dir()
+    let mut folders_list: [char; 0] = [];
+    let xci_path = std::env::current_dir()
         .unwrap()
-        .join("**/games/**/*.*")
+        .join("**/games/**/*.xci")
         .to_str()
         .unwrap_or("")
         .to_string();
-    for entry in glob(&path_to_search)
-        .expect("Failed to read glob pattern")
-        .filter_map(Result::ok)
+    let nsz_path = std::env::current_dir()
+        .unwrap()
+        .join("**/games/**/*.nsz")
+        .to_str()
+        .unwrap_or("")
+        .to_string();
+    for file_name_result in glob(&xci_path)
+        .unwrap()
+        .chain(glob(&nsz_path).unwrap())
     {
-        if entry.is_dir() {
-            dirs.push(entry);
-        } else if entry.is_file() && !entry.ends_with("nsz") {
-            files.push(entry);
+        match file_name_result {
+            Ok(file_path) => {
+                if file_path.is_dir() {
+                    dirs.push(file_path);
+                } else if file_path.is_file() && !file_path.ends_with("nsz") {
+                    files.push(file_path);
+                }
+            }
+            Err(e) => {
+                eprintln!("ERROR FILE SEARCH: {}", e)
+            }
         }
     }
     return Ok(TinfoilResponse {
@@ -73,8 +85,11 @@ async fn list_roms_files() -> Result<TinfoilResponse, Error> {
 
 async fn build_json(parsed_template: Value) -> String {
     let files_found = list_roms_files().await.unwrap();
+    println!("");
     println!("listed files: {:?}", files_found.files);
+    println!("");
     println!("listed directories: {:?}", files_found.directories);
+    println!("");
     let mut template = parsed_template;
     template["directories"] = serde_json::json!(files_found.directories);
     template["files"] = serde_json::json!(files_found.files);
